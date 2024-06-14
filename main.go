970b282b042b8c0f70ba2d5ac006d7d92293aa36
Dyam Marcano <dyam.marcano@gmail.com>
2024-06-14 14:08:01 -0300

    add content type, cache and fix issues

--- main.go
+++ main.go
@@ -1,23 +1,21 @@
 package main
 
 import (
-	"context"
 	"embed"
 	"fmt"
+	"log/slog"
 	"net/http"
+	"os"
 	"strings"
-	"time"
 )
 
-const assetsDir = "dist/project/browser"
+//go:generate ng build
 
-//go:embed dist/project/browser/*
-var assets embed.FS
+const assetsDir = "project/browser"
 
-type AssetsCache struct {
-	ctx    context.Context
-	assets map[string]*Asset
-}
+//go:embed project/browser/*
+var afs embed.FS
+var assets = make(map[string]*Asset)
 
 type Asset struct {
 	Name        string
@@ -26,90 +24,31 @@ type Asset struct {
 	TTL         int
 }
 
-func newAssetsCache(ctx context.Context) *AssetsCache {
-	a := &AssetsCache{
-		ctx:    ctx,
-		assets: make(map[string]*Asset),
-	}
-
-	a.checkTTL()
-
-	return a
-}
-func (ac *AssetsCache) checkTTL() {
-	go func() {
-		for {
-			select {
-			case <-ac.ctx.Done():
-				return
-			default:
-				for k, v := range ac.assets {
-					if v.TTL < time.Now().Second() {
-						delete(ac.assets, k)
-					}
-				}
-				time.Sleep(1 * time.Minute)
-			}
-		}
-	}()
-}
-
-func (ac *AssetsCache) GetAsset(name string) (*Asset, error) {
-	if strings.Contains(name, "..") {
-		return nil, fmt.Errorf("Invalid file path")
-	}
-
+func getAsset(name string) (*Asset, error) {
 	if strings.Contains(name, "/") {
 		name = strings.TrimPrefix(name, "/")
 	}
 
-	path, ok := ac.assets[name]
+	path, ok := assets[name]
 	if ok {
 		return path, nil
 	}
 
-	content, err := assets.ReadFile(fmt.Sprintf("%s/%s", assetsDir, name))
+	content, err := afs.ReadFile(fmt.Sprintf("%s/%s", assetsDir, name))
 	if err != nil {
 		return nil, err
 	}
 
-	ac.assets[name] = &Asset{
+	assets[name] = &Asset{
 		Name:        name,
 		Content:     content,
-		TTL:         time.Now().Add(12 * time.Hour).Second(),
-		ContentType: detectContentType(name),
+		ContentType: contentType(name),
 	}
 
-	return ac.assets[name], nil
+	return assets[name], nil
 }
 
-func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	var cache = newAssetsCache(ctx)
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fileName := r.URL.Path
-		if fileName == "/" {
-			fileName = "index.html"
-		}
-
-		// Get the file from the assets cache
-		asset, err := cache.GetAsset(fileName)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		// Write the file to the response
-		w.Header().Set("Content-Type", asset.ContentType)
-		w.Write(asset.Content)
-	})
-
-	http.ListenAndServe(":8080", nil)
-}
-
-func detectContentType(filename string) string {
+func contentType(filename string) string {
 	switch {
 	case strings.HasSuffix(filename, ".html"):
 		return "text/html"
@@ -134,4 +73,33 @@ func detectContentType(filename string) string {
 	default:
 		return "application/octet-stream"
 	}
+}
+
+func init() {
+	logger := slog.New(slog.NewJSONHandler(os.Stdout, nil))
+	slog.SetDefault(logger)
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fileName := r.URL.Path
+		if fileName == "/" {
+			fileName = "index.html"
+		}
+
+		// Get the file from the assets cache
+		asset, err := getAsset(fileName)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		// Write the file to the response
+		w.Header().Set("Content-Type", asset.ContentType)
+		w.Write(asset.Content)
+	})
+
+	slog.Info("Server started on port 8080")
+
+	http.ListenAndServe(":8080", nil)
 }
